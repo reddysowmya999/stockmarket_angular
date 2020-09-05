81f5aa1c78a8c889f8ba8b40c959279a7f0262f2
reddysowmya999 <43107848+reddysowmya999@users.noreply.github.com>
2020-09-05 23:18:20 +0530

    Update otheruser.component.ts

--- src/app/otheruser/otheruser.component.ts
+++ src/app/otheruser/otheruser.component.ts
@@ -18,30 +18,40 @@ export class OtheruserComponent implements OnInit {
   updatepage: boolean = true;
   success: boolean;
   processend: boolean = false;
-  
+  pass_val:RegExp = new RegExp("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}");
+  email_val:RegExp = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$");
 
   constructor(private loginserviceService: LoginserviceService,
     private router: Router,
     private userService : UserServiceService){
       this.user= new User();
   }
+  
   async onSubmit(){
     let res;
-    res= await this.userService.updateUser (this.user.userName,this.user.password,
-      this.user.email);
-      if(res==null){
-        this.updatepage=false;
-        this.processend=true;
-        this.success = false;
-        console.log("failure");
-      }
-      else{
-        this.updatepage=false;
-        this.processend=true;
-        this.success = true;
-        
-      }
+    if(!this.user.password.match(this.pass_val)){
+      alert("Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more ");
     }
+    else if(!this.user.email.match(this.email_val)){
+      alert(" Enter valid email");
+    }
+    else{
+        res= await this.userService.updateUser (this.user.userName,this.user.password,
+        this.user.email);
+        if(res==null){
+          this.updatepage=false;
+          this.processend=true;
+          this.success = false;
+          console.log("failure");
+        }
+        else{
+          this.updatepage=false;
+          this.processend=true;
+          this.success = true;
+
+        }
+    }
+ }
  
 
   getLoginDetails(): string {
