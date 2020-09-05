d435cb850b7f6ccba9e1dec74c6360495c224108
Amlan66 <36122995+Amlan66@users.noreply.github.com>
2020-09-05 13:42:40 +0530

    Add files via upload

---
+++ src/app/classes/User.ts
@@ -0,0 +1,11 @@
+//added for signup and update functionalities Sowmya
+export class User{
+    userName:string;
+    password: string;
+    email:string;
+    isAdmin:string;
+    userId: number;
+    isVerified:boolean;
+
+  
+}
