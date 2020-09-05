1c7a8207093a9db52da0427c172e18cca077901b
reddysowmya999 <43107848+reddysowmya999@users.noreply.github.com>
2020-09-05 23:08:46 +0530

    Update signup.component.ts

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -18,6 +18,9 @@ export class SignupComponent {
   result : string;
   success: boolean;
   processend: boolean = false;
+  pass_val:RegExp = new RegExp("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}");
+  email_val:RegExp = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$");
+  
 
   constructor(private loginserviceService: LoginserviceService,
     private router: Router,
@@ -28,22 +31,29 @@ export class SignupComponent {
 
   async onSubmit(){
     let res;
-   res= await this.userService.addUser (this.user.userName,this.user.password,this.user.email);
-   if(res == null){
-     this.signuppage=false;
-     this.processend=true;
-     this.success = false;
-   }
-   else{
-     
-     this.signuppage=false;
-     this.processend=true;
-     this.success = true;
- 
- 
-   }
+    if(!this.user.password.match(this.pass_val)){
+      alert("Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more");
+     }
+    else if(!this.user.email.match(this.email_val)){
+      alert(" Enter valid email");
+     }
+    else{
+        res= await this.userService.addUser (this.user.userName,this.user.password,this.user.email);
+        if(res == null){
+         this.signuppage=false;
+         this.processend=true;
+         this.success = false;
+        }
+        else{
+         this.signuppage=false;
+         this.processend=true;
+         this.success = true;
+        }
+    }
  
   }
+  
+  
   onsuccess(){
     this.loginserviceService.loggedin("user");
     this.router.navigate(['/userlanding']);
