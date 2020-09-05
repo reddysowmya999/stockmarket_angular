4ae3bff30cd7544c8ee64fe28264f553c0b2b04d
Amlan66 <36122995+Amlan66@users.noreply.github.com>
2020-09-05 13:47:41 +0530

    Add files via upload

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,21 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginserviceService} from '../loginservice.service';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { User }  from './../classes/user';
+import { UserServiceService }  from './../user-service.service';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+
+export class SignupComponent {
+
+  signuppage:boolean=true;
+  user: User;
+  result : string;
+  success: boolean;
+  processend: boolean = false;
 
   constructor(private loginserviceService: LoginserviceService,
-    private router: Router){
+    private router: Router,
+    private  userService: UserServiceService){
+
+      this.user= new User();
   }
 
-  ngOnInit(): void {
+  async onSubmit(){
+    let res;
+   res= await this.userService.addUser (this.user.userName,this.user.password,this.user.email);
+   if(res == null){
+     this.signuppage=false;
+     this.processend=true;
+     this.success = false;
+   }
+   else{
+     
+     this.signuppage=false;
+     this.processend=true;
+     this.success = true;
+ 
+ 
+   }
+ 
   }
-
   onsuccess(){
     this.loginserviceService.loggedin("user");
     this.router.navigate(['/userlanding']);
